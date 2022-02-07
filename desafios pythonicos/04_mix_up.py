30f473a3fd387db1acd31cbd07b1c4c5dec8d0ec
Hugo Penna <git@hugopenna.com>
2022-02-07 08:26:42 -0300

    Resolve mix up

--- desafios pythonicos/04_mix_up.py
+++ desafios pythonicos/04_mix_up.py
@@ -13,8 +13,8 @@ Assuma que a e b tem tamanho 2 ou maior.
 """
 
 def mix_up(a, b):
-    # +++ SUA SOLUÇÃO +++
-    return
+    stra = b[:2] + a[2:] + ' ' + a[:2] + b[2:]
+    return stra
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
