c91e45c0785a1aebc9c2b3cbd60667ca9b927ebb
Hugo Penna <10089299+hugopenna@users.noreply.github.com>
2022-03-14 09:23:44 -0300

    wip fizzbuzz.py

---
+++ fizzbuzz.py
@@ -0,0 +1,28 @@
+"""
+Regras Fizzbuzz
+
+1. Se a posição for múltipla de 3: fizz
+2. Se a posição for múltipla de 5: buzz
+3. Se a posição for múltipla de 3 e 5: fizzbuzz
+4. Para qualquer outra posição fale o próprio nº
+"""
+
+def robot(pos):
+    if pos % 5 == 0:
+        return 'buzz'
+    if pos % 3 == 0:
+        return 'fizz'
+    return str(pos)
+
+if __name__ == '__main__':
+    assert robot(1) == '1'
+    assert robot(2) == '2'
+    assert robot(4) == '4'
+
+    assert robot(3) == 'fizz'
+    assert robot(6) == 'fizz'
+    assert robot(9) == 'fizz'
+
+    assert robot(5) == 'buzz'
+    assert robot(10) == 'buzz'
+    assert robot(20) == 'buzz'
